96d69518fcc09339a5108c082ef88895d41b37d8
Mariana <marianaalmeidafga@gmail.com>
2023-05-13 09:28:05 -0300

    novas atualizações

--- lib/view/home/principal.home.dart
+++ lib/view/home/principal.home.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:tcc_girls_in_ctrl/view/home/carrossel.home.dart';
 import 'package:tcc_girls_in_ctrl/view/home/info.home.dart';
-import 'package:tcc_girls_in_ctrl/view/menus/bottonNavigator.menus.dart';
 
 class Principal extends StatefulWidget {
   @override
