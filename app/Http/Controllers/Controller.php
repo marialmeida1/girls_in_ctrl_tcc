fbb094f4126919fe08725619abcbac8986ded320
girls-in-crtl.domcloud.io <girls-in-crtl@nyc.domcloud.id>
2023-05-25 00:00:55 +0000

    adicionando parametros de imagem

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Storage;
@@ -17,20 +18,18 @@ class Controller extends BaseController
     // $path define o caminho, caso nao tenha um caminho definido, ele vai para public para via das duvidas
     public function saveImage($image, $path = 'public')
     {
-
         // retorna nulo caso nao seja adicionado imagem
         if (!$image) {
             return null;
         }
-
         // salva a imagem de maneira unica de acordo com o tempo que foi salva
-        $filename = time() + '.png';
+        $filename = time() . '.png';
 
         // define o local que a imagem vai ser salva, e faz um "put"(atualiza) o local
         // o base64_decode serve como um decodificador binario do código que foi enviado da imagem
         Storage::disk($path)->put($filename, base64_decode($image));
 
         // salva a imagem no local estipulado
-        return URL::to('/' . '/storege/' .  $path . '/' . $filename);
+        return URL::to('/') . '/storage/' . $path . '/' . $filename;
     }
 }
